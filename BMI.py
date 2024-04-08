fd981cf5a689304704538d23bac511be641a7c71
TESANCAANGGARA <TESANCAANGGARAHONG@gmail.com>
2024-04-08 11:19:50 +0800

    test2

--- BMI.py
+++ BMI.py
@@ -6,14 +6,14 @@ sum = 0
 while True : 
     # 1. Input
     x1 = input ("Enter your weight in kilograms  : ")
-    x2 = input ("Enter your height in meters    : ")
+    x2 = input ("Enter your height in meters     : ")
 
     # 2. Process
     sum =  float (x1) / (float (x2)**2)
 
     # 3. Output
     print (f"Your BMI is : {sum} ")
-    res = input ("Continue? (yes/no)  : ")
+    res = input ("Continue? (yes/no) : ")
     if res == "no" :  
         break;
 
