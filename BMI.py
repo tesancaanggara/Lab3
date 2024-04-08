7c435a4437412b29bc2745d7e439fb9393f8c480
TESANCAANGGARA <TESANCAANGGARAHONG@gmail.com>
2024-04-08 11:16:00 +0800

    test

---
+++ BMI.py
@@ -0,0 +1,19 @@
+# Tesanca Anggara
+# BMI
+
+sum = 0 
+
+while True : 
+    # 1. Input
+    x1 = input ("Enter your weight in kilograms  : ")
+    x2 = input ("Enter your height in meters    : ")
+
+    # 2. Process
+    sum =  float (x1) / (float (x2)**2)
+
+    # 3. Output
+    print (f"Your BMI is : {sum} ")
+    res = input ("Continue? (yes/no)  : ")
+    if res == "no" :  
+        break;
+
