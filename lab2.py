c542870694aad78d5ccbf135dd62af96efe81551
TESANCAANGGARA <TESANCAANGGARAHONG@gmail.com>
2024-04-22 14:19:50 +0800

    test

--- lab2.py
+++ lab2.py
@@ -5,17 +5,14 @@
 
 # Define Function
 def cal(x1, x2, op):
-    x1 = float(x1)
-    x2 = float(x2)
-
     if op == '+':
-        return int(x1) + (x2)
+        return int(x1) + int(x2)
     elif op == '-':
-        return int(x1) - (x2)
+        return int(x1) - int(x2)
     elif op == '*':
-        return int(x1) * (x2)
+        return int(x1) * int(x2)
     elif op == '/':
-        return int(x1) / (x2)
+        return int(x1) / int(x2)
 
 # 1. Input
 x1 = input('Type x1: ')
@@ -23,7 +20,7 @@ x2 = input('Type x2: ')
 op = input('Type operator: ')
 
 # 2. Process
-sum = cal(x1,x2,op)
+sum = cal(x1, x2, op)
 
 # 3. Output
 print(f'Sum: {sum}')
